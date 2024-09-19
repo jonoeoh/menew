Indie365/rails

Empty embeds when RichText is set to an empty content
Fix #51269

Action Text supports saving a RichText model with empty content. Passing _almost_ empty content (such as a space) flushes the embeds, but passing truly empty content does not. This defies expectations.

This change ensures that the embeds are flushed when empty content is passed.

actiontext/app/models/action_text/rich_text.rb
@@ -52,7 +52,7 @@ module ActionText
     has_many_attached :embeds
 
     before_save do
-      self.embeds = body.attachables.grep(ActiveStorage::Blob).uniq if body.present?
+      self.embeds = body.nil? ? [] : body.attachables.grep(ActiveStorage::Blob).uniq
     end
 
     # Returns a plain-text version of the markup contained by the `body` attribute,

jonoeoh/menew

actiontext/app/models/action_text/rich_text.rb
@@ -52,7 +52,7 @@ module ActionText
     has_many_attached :embeds
 
     before_save do
-      self.embeds = body.attachables.grep(ActiveStorage::Blob).uniq if body.present?
+      self.embeds = body.blank? ? [] : body.attachables.grep(ActiveStorage::Blob).uniq
     end
 
     # Returns a plain-text version of the markup contained by the `body` attribute,