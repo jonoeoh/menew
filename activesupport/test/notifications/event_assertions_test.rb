09c2ec158049634ca5628048b0897ea3afc2437d
Nicholas La Roux <larouxn@gmail.com>
2024-09-29 09:07:24 +0000

    Clean up EventAssertionsTest using assert_raises match argument

--- activesupport/test/notifications/event_assertions_test.rb
+++ activesupport/test/notifications/event_assertions_test.rb
@@ -17,25 +17,22 @@ module ActiveSupport
           ActiveSupport::Notifications.instrument("post.submitted", title: "Cool Post")
         end
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /No post.submitted events were found/) {
           assert_event("post.submitted", title: "Cool Post") { nil } # no events
         }
-        assert_match(/No post.submitted events were found/, error.message)
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /No post.submitted event with payload {:title=>"Cool Post"} was found. Did you mean one of these payloads: {:title=>"Cooler Post"}/) {
           assert_event("post.submitted", title: "Cool Post") do
             ActiveSupport::Notifications.instrument("post.submitted", title: "Cooler Post")
           end
         }
-        assert_match(/No post.submitted event with payload {:title=>"Cool Post"} was found. Did you mean one of these payloads: {:title=>"Cooler Post"}/, error.message)
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /No post.submitted event with payload {:title=>"Cool Post"} was found. Did you mean one of these payloads: {:title=>"Cooler Post"}\n{:title=>"Coolest Post"}/) {
           assert_event("post.submitted", title: "Cool Post") do
             ActiveSupport::Notifications.instrument("post.submitted", title: "Cooler Post")
             ActiveSupport::Notifications.instrument("post.submitted", title: "Coolest Post")
           end
         }
-        assert_match(/No post.submitted event with payload {:title=>"Cool Post"} was found. Did you mean one of these payloads: {:title=>"Cooler Post"}\n{:title=>"Coolest Post"}/, error.message)
       end
 
       def test_assert_events_count
@@ -43,29 +40,26 @@ module ActiveSupport
           ActiveSupport::Notifications.instrument("post.submitted", title: "Cool Post")
         end
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /Expected 1 instead of 2 events for post.submitted/) {
           assert_events_count("post.submitted", 1) do
             ActiveSupport::Notifications.instrument("post.submitted", title: "Cool Post")
             ActiveSupport::Notifications.instrument("post.submitted", title: "Cooler Post")
           end
         }
-        assert_match(/Expected 1 instead of 2 events for post.submitted/, error.message)
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /Expected 1 instead of 0 events for post.submitted/) {
           assert_events_count("post.submitted", 1) { nil } # no events
         }
-        assert_match(/Expected 1 instead of 0 events for post.submitted/, error.message)
       end
 
       def test_assert_no_events
         assert_no_events("post.submitted") { nil } # no events
 
-        error = assert_raises(Minitest::Assertion) {
+        assert_raises(Minitest::Assertion, match: /Expected no events for post.submitted but found 1/) {
           assert_no_events("post.submitted") do
             ActiveSupport::Notifications.instrument("post.submitted", title: "Cool Post")
           end
         }
-        assert_match(/Expected no events for post.submitted but found 1/, error.message)
       end
     end
   end
